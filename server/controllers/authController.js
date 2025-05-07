47a2d24b4c2f319d900c4fb9d8f2b19a1c38f182
Jikson Mathew <153893387+jiksonmathew@users.noreply.github.com>
2025-05-07 12:07:52 +0530

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -4,12 +4,12 @@ const jwt = require("jsonwebtoken");
 
 const generateToken = (res, id) => {
   const token = jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "7d" });
-  res.cookie("token", token, {
-    httpOnly: true,
-    secure: true,
-    sameSite: "None",
-    maxAge: 7 * 24 * 60 * 60 * 1000,
-  });
+ res.cookie("token", token, {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+});
 };
 
 exports.register = async (req, res) => {
