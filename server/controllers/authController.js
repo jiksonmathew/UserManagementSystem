c13f90b1f26ffbc78c26bce962a53cbcdfe04e04
Jikson Mathew <153893387+jiksonmathew@users.noreply.github.com>
2025-05-07 11:59:49 +0530

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -7,7 +7,7 @@ const generateToken = (res, id) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: true,
-    sameSite: "strict",
+    sameSite: "None",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 };
