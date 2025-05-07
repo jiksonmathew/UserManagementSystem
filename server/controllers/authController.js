5d959b087460573c5493583930b1e1db2c0d19a3
Jikson Mathew <153893387+jiksonmathew@users.noreply.github.com>
2025-05-07 21:04:44 +0530

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -6,8 +6,8 @@ const generateToken = (res, id) => {
   const token = jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "7d" });
   res.cookie("token", token, {
     httpOnly: true,
-    secure: false,
-    sameSite: "strict",
+    secure: true,
+    sameSite: "None",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 };
@@ -47,8 +47,8 @@ exports.login = async (req, res) => {
 exports.logout = (req, res) => {
 res.clearCookie("token", {
   httpOnly: true,
-  secure: true,         // Must be true in production (HTTPS)
-  sameSite: "None",     // Required for cross-origin cookie clearing
+  secure: true,  
+  sameSite: "None",
 });
 res.status(200).json({ message: "Logged out successfully" });
 
